12ecac12e02839c8c4c8291babe497064e824ea2
Graham Plata <graham.plata@gmail.com>
2016-09-07 16:52:50 -0400

    iteration through rows of data and writing to a string

--- timeline.rb
+++ timeline.rb
@@ -1,19 +1,38 @@
-# JSON Parsing example
-require "rubygems"
-require "json"
+#!/usr/bin/ruby
+require 'rubygems'
+require 'json'
+require 'pp'
+require 'erb'
 
-string = '{"desc":{"someKey":"someValue","anotherKey":"value"},"main_item":{"stats":{"a":8,"b":12,"c":10}}}'
-parsed = JSON.parse(string) # returns a hash
+# Read JSON from a file
+file = File.read('rosters.json')
+data_hash = JSON.parse(file)
 
-p parsed["desc"]["someKey"]
-p parsed["main_item"]["stats"]["a"]
+#generating proper english based on event given
+def player_action(input)
+  if input == "Released from team"
+    return input.insert(0, 'was ').downcase
+  elsif input == "Automatically dropped (Sub)"
+    return input = "was automatically dropped"
+  elsif input == "Left the team"
+    return input.insert(0, 'has ').downcase
+  elsif input == "Joined the team"
+    return input.downcase
+  else
+    return input
+  end
+end
 
-# Read JSON from a file, iterate over objects
-file = open("rosters.json")
-json = file.read
+#iterate through hash
+data_hash.each do |key|
+  player = key['player_name']
+  team = key['team_name']
+  teamID = key['team_id']
+  date = key['date']
+  time = key['time']
+  action = key['action']
 
-parsed = JSON.parse(json)
-
-parsed["shop"].each do |shop|
-  p shop["id"]
+  #debugging lines
+  puts player + " " + player_action(action) + ": #{team}" + " on " + date + " on " + time
+#  puts '-' * 20
 end
